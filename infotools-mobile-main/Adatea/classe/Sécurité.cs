c62250eee5b9d66de946333919ae8a8e04427d77
AlbanSIO <168306693+AlbanSIO@users.noreply.github.com>
2024-05-22 19:42:10 +0200

    MAUI

---
+++ infotools-mobile-main/Adatea/classe/Sécurité.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace Adatea.classe
+{
+    public static class Sécurité
+    {
+        public static string HashPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                string hash = BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+                return hash;
+            }
+        }
+    }
+}
